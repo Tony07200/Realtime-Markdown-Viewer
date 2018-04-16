23677ce79259eded8a7715e2d03c43ca0ef1557c
Tony07200 <baptiste.anthony07@gmail.com>
2018-04-16 12:12:48 +0200

    Revert "add image parser"
    This reverts commit c9cb4daeab21ccd40489d8d963c99fac5192678f.

--- converter/markdown.js
+++ converter/markdown.js
@@ -1,66 +1,59 @@
-var parseHeadline = function (str) {
-    var headlineRegExp = /^(\#{1,6})([^\#\n]+)$/m;
-    var stra = [];
-    while ((stra = headlineRegExp.exec(str)) !== null) {
-        count = stra[1].length;
-        str = str.replace(stra[0], '<h' + count + '>' + stra[2] + '</h' + count + '>' + '\n');
-    }
-    return str;
+ var parseHeadline = function(str) {
+   var headlineRegExp =  /^(\#{1,6})([^\#\n]+)$/m;
+   var stra = [];
+   while ((stra = headlineRegExp.exec(str)) !== null) {
+     count = stra[1].length;
+     str = str.replace(stra[0], '<h' + count + '>' + stra[2] + '</h' + count + '>' + '\n');
+   }
+   return str;
+ }
+
+ var parseItalic = function(str) {
+  var italicRegExp = /(\*|_)(.*?)\1/;
+  var stra = [];
+  while ((stra = italicRegExp.exec(str)) !== null) {
+    str = str.replace(stra[0], '<i>' + stra[2] + '</i>')
+  }
+  return str;
 }
 
-var parseItalic = function (str) {
-    var italicRegExp = /(\*|_)(.*?)\1/;
-    var stra = [];
-    while ((stra = italicRegExp.exec(str)) !== null) {
-        str = str.replace(stra[0], '<i>' + stra[2] + '</i>')
-    }
-    return str;
-}
-
-
-var parseBold = function (str) {
-    var boldRegExp = /(\*\*)(.*?)\1/;
-    var stra = [];
-    while ((stra = boldRegExp.exec(str)) !== null) {
-        str = str.replace(stra[0], '<b>' + stra[2] + '</b>')
-    }
-    return str;
-}
-
-
-var parseStrong = function (str) {
-    var strongRegExp = /(~~)(.*?)\1/;
-    var stra = [];
-    while ((stra = strongRegExp.exec(str)) !== null) {
-        str = str.replace(stra[0], '<strong>' + stra[2] + '</strong>')
-    }
+var parseBold = function(str) {
+  var boldRegExp = /(\*\*)(.*?)\1/;
+  var stra = [];
+  while ((stra = boldRegExp.exec(str)) !== null) {
+    str = str.replace(stra[0], '<b>' + stra[2] + '</b>')
+  }
     return str;
 }
 
 
-var parseHorizontaleLine = function (str) {
-    var horizontalRegExp = /^(?:([\*\-_] ?)+)\1\1$/gm;
-    var stra = [];
-    while ((stra = horizontalRegExp.exec(str)) !== null) {
-        str = str.replace(stra[0], '\n<hr/>\n');
-    }
-    return str;
+var parseStrong = function(str) {
+  var strongRegExp = /(~~)(.*?)\1/;
+  var stra = [];
+  while ((stra = strongRegExp.exec(str)) !== null) {
+    str = str.replace(stra[0], '<strong>' + stra[2] + '</strong>')
+  }
+  return str;
 }
 
-var parseLink = function (str) {
-    var linkRegExp = /\[([^\[]+)\]\(([^\)]+)\)/;
-    var stra = [];
-    while ((stra = linkRegExp.exec(str)) !== null) {
-        str = str.replace(stra[0], '<a ' + 'href="' + stra[2] + '">' + stra[1] + '</a>');
-        if (stra[0].substr(0, 1) === '!') {
-            str = str.replace(stra[0], '<img src="' + stra[2] + '" alt="' + stra[1] + '" title="' + stra[1] + '" />\n');
-        } else {
-            str = str.replace(stra[0], '<a ' + 'href="' + stra[2] + '">' + stra[1] + '</a>');
-        }
 
-    }
-    return str;
-}
+ var parseHorizontaleLine = function(str) {
+  var horizontalRegExp = /^(?:([\*\-_] ?)+)\1\1$/gm;
+  var stra = [];
+  while ((stra = horizontalRegExp.exec(str)) !== null) {
+    str = str.replace(stra[0], '\n<hr/>\n');
+  }
+  return str;
+ }
+
+ var parseLink = function(str) {
+  var linkRegExp = /\[([^\[]+)\]\(([^\)]+)\)/;
+  var stra = [];
+  while ((stra = linkRegExp.exec(str)) !== null) {
+    str = str.replace(stra[0], '<a ' + 'href="' + stra[2] + '">' + stra[1] + '</a>');
+  }
+  return str;
+ }
 
 var parseNewLine = function(str) {
   var newLineRegExp = /(^|[^\n])\n{1}(?!\n)/;
@@ -81,28 +74,26 @@ var parseNewLine = function(str) {
      return str;
  }
 
+ var parseBlockQuote = function(str) {
+  var quoteRegExp = /\:\"(.*?)\"\:/
+  var stra = [];
+  while ((stra = quoteRegExp.exec(str)) !== null) {
+    str = str.replace(stra[0], '<blockquote>' + stra[1] + '</blockquote>');
+  }
+  return str;
+ }
 
-var parseBlockQuote = function (str) {
-    var quoteRegExp = /\:\"(.*?)\"\:/
-    var stra = [];
-    while ((stra = quoteRegExp.exec(str)) !== null) {
-        str = str.replace(stra[0], '<blockquote>' + stra[1] + '</blockquote>');
-    }
-    return str;
-}
-
-var parseDel = function (str) {
-    var delRegExp = /\{(.*?)\}/
-    var stra = [];
-    while ((stra = delRegExp.exec(str)) !== null) {
-        str = str.replace(stra[0], '<del>' + stra[1] + '</del>');
-    }
-    return str;
-}
+ var parseDel = function(str) {
+  var delRegExp = /\{(.*?)\}/
+  var stra = [];
+  while ((stra = delRegExp.exec(str)) !== null) {
+    str = str.replace(stra[0], '<del>' + stra[1] + '</del>');
+  }
+  return str;
+ }
 
 
 var markdown = {
-
   parse: function (str, strict) {
     'use strict';
     str = parseNewLine(str);
@@ -118,7 +109,6 @@ var markdown = {
 
     return str;
   }
-
 };
 
 module.exports = markdown;
